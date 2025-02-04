23bdeac08ba3a5fbc8e3b7ea4f19f116e97c2aa0
Grytodin <98953123+Grytodin@users.noreply.github.com>
2025-02-04 21:00:41 +0200

    Update snake.js

--- src/commands/fun/snake.js
+++ src/commands/fun/snake.js
@@ -75,7 +75,7 @@ async function startSnakeGame(data, isInteraction) {
     let embed = new EmbedBuilder().setTitle("Snake Game").setDescription(renderBoard(board));
     let msg = await data.channel.send({ embeds: [embed], components: [createButtons()] });
 
-    let collector = msg.createMessageComponentCollector({ componentType: ComponentType.Button, time: 60000 });
+    let collector = msg.createMessageComponentCollector({ componentType: ComponentType.Button, time: 300000 });
 
     collector.on('collect', async interaction => {
         if (interaction.user.id !== (isInteraction ? data.user.id : data.author.id)) {
