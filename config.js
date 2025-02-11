9e82f69986c1c2148e32e546c567ac18dc361c25
Grytodin <98953123+Grytodin@users.noreply.github.com>
2025-02-11 14:09:02 +0200

    Update config.js

--- config.js
+++ config.js
@@ -104,7 +104,7 @@ module.exports = {
     ENABLED: true, // Enable bot presence
     STATUS: "idle", // Bot status [online, idle, dnd, invisible]
     TYPE: "WATCHING", // Presence type [PLAYING, LISTENING, WATCHING, COMPETING]
-    MESSAGE: "ATTENTION!!! The bot is still in development.The release stage will be published on the support server below.", // Status message
+    MESSAGE: "ATTENTION!!! The bot is still in development.The release stage will be published on the support server below.{members} members in (servers) servers", // Status message
   },
 
   STATS: {
